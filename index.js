25e860278bc5c6ea5943ce2b6c6dc015026f53bc
MeinSpaghett <minhannguyencao@gmail.com>
2019-07-25 22:35:47 +0700

    This is Project Scarlet

---
+++ index.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+AOS.init();
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+  
+=======
+AOS.init();
+>>>>>>> 5e92df9c606361c2e7b61881a2ce7e522438bdf2
