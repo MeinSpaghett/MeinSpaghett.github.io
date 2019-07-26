2b8745acfaffd0e18e5f66ace6a6fa2ba68f6620
MeinSpaghett <minhannguyencao@gmail.com>
2019-07-26 15:01:58 +0700

    Scarlett

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+function unavailable() {
+    alert("This feature is currently unavailable and our team will update this feature soon.")
+}
   
